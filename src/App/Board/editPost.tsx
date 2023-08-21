ee6eccd12d302613524d51cdb0de19173c848d23
ur <dorosi150@gmail.com>
2023-08-21 22:37:22 +0900

    Feat: Add poll in editPost.tsx

--- src/App/Board/editPost.tsx
+++ src/App/Board/editPost.tsx
@@ -1,13 +1,16 @@
+import { AntDesign } from "@expo/vector-icons";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import CheckBox from "expo-checkbox";
+import * as ImagePicker from "expo-image-picker";
 import React, { useEffect, useState } from "react";
-import { Text } from "react-native";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 import { boardEdit } from "../../Api/board";
-import { BoardType } from "../../App/Board/BoardList";
 import { Container, Input, Spacer, TextButton } from "../../components/common";
 import { NavigationProps } from "../../Navigator/Routes";
-import { BoardPost } from "../../types/Board";
+import { BoardPost, PollOptions } from "../../types/Board";
+import { uploadAPI } from "../../utils/Image";
 
 const Edit: React.FC = () => {
   const [title, setTitle] = useState<string>("");
@@ -17,6 +20,14 @@ const Edit: React.FC = () => {
   const [isAnonymous, setIsAnonymous] = useState<number>(0);
   const [hashtagInput, setHashtagInput] = useState("");
   const [hashtags, setHashtags] = useState<string[]>([]);
+  const [voteInputs, setVoteInputs] = useState(false);
+  const [voteTitle, setVoteTitle] = useState<string>("");
+  const [voteOptions, setVoteOptions] = useState<PollOptions[]>([]);
+
+  const [requsetpermission, setRequestPermission] = ImagePicker.useCameraPermissions();
+  const [imageUrl, setImageUrl] = useState<string[]>([]);
+
+  const [postImageUrl, setPostImageUrl] = useState<string[]>([]);
 
   const navigation = useNavigation<NavigationProps>();
 
@@ -43,11 +54,106 @@ const Edit: React.FC = () => {
   const handleUpdate = async () => {
     const DBnewHashtags = hashtags.map(word => word.replace(/^#/, ""));
     try {
-      const response = await boardEdit(post.board.id, title, body, isHide);
+      const response = await boardEdit(post.board.id, title, body, isHide, tags);
       console.log(response);
       navigation.goBack();
     } catch (error) {
       console.error(error);
+    }
+  };
+  const updateHashtags = () => {
+    const words = hashtagInput.split(" ");
+    console.log("words", words);
+    const newHashtags = words.filter(word => word.startsWith("#"));
+    console.log("newHashtags ", newHashtags);
+    setHashtags(newHashtags);
+  };
+
+  const addHashtag = () => {
+    if (hashtagInput.trim() !== "") {
+      updateHashtags();
+      setHashtagInput("");
+    }
+  };
+
+  const voteHandling = async () => {
+    setVoteInputs(!voteInputs);
+  };
+
+  const addVoteOptions = async () => {
+    setVoteOptions([...voteOptions, ""]);
+  };
+
+  const handleVoteOptionChange = (index: number, text: string) => {
+    const updatedOptions = [...voteOptions];
+    updatedOptions[index] = text;
+    setVoteOptions(updatedOptions);
+  };
+
+  const handleRemoveVoteOption = (indexToRemove: number) => {
+    setVoteOptions(voteOptions.filter((_, index) => index !== indexToRemove));
+  };
+
+  const uploadImage = async () => {
+    console.log("사진 올려지나여");
+    if (!requsetpermission?.granted) {
+      const permission = await setRequestPermission();
+      if (!permission.granted) {
+        return null;
+      }
+    }
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsMultipleSelection: true,
+      quality: 1,
+    });
+
+    //이미지 업로드 취소시
+    if (result.canceled) {
+      return null;
+    }
+    const selectedImages = result.assets.slice(0, 5);
+
+    try {
+      // Call the uploadImageAsync function to upload the selected images
+      await uploadImageAsync(selectedImages);
+    } catch (error) {
+      console.error("Image upload error:", error);
+      // Handle the error
+    }
+    setImageUrl(selectedImages.map(image => image.uri));
+
+    console.log("selectedImages", selectedImages);
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async function uploadImageAsync(images: any[]) {
+      const formData = new FormData();
+      console.log(images);
+
+      images.forEach((image, index) => {
+        // @ts-expect-error
+        formData.append("files", {
+          uri: image.uri,
+          type: "image/jpeg",
+          name: `image_${index + 1}.jpg`,
+        });
+      });
+
+      console.log(formData);
+
+      try {
+        // const response = await uploadAPI(formData);
+        const response = uploadAPI("POST", "/image", formData);
+        console.log("Image upload response:", (await response).json);
+        const jsonResponse = (await response).json;
+        for (const item of jsonResponse) {
+          const imageUrl = item.url;
+          postImageUrl.push(imageUrl);
+        }
+        setPostImageUrl(postImageUrl);
+      } catch (error) {
+        console.error("Image upload error:", error);
+      }
     }
   };
 
@@ -80,6 +186,16 @@ const Edit: React.FC = () => {
           </Container>
         </Container>
         <Container style={{ flexDirection: "row" }}>
+          <TouchableOpacity onPress={voteHandling}>
+            <AntDesign name="cloud" size={24} color="black" />
+          </TouchableOpacity>
+        </Container>
+        <Container style={{ flexDirection: "row" }}>
+          <TouchableOpacity onPress={uploadImage}>
+            <AntDesign name="camerao" size={24} color="black" />
+          </TouchableOpacity>
+        </Container>
+        <Container style={{ flexDirection: "row" }}>
           <TextButton onPress={handleUpdate}>완료</TextButton>
         </Container>
       </Container>
@@ -91,6 +207,57 @@ const Edit: React.FC = () => {
           multiline={true}
         ></Input>
         <Spacer size={20} />
+        <TextInput
+          placeholder="#해시태그 형식을 지켜주세요."
+          onChangeText={text => setHashtagInput(text)}
+          value={hashtagInput}
+          multiline={true}
+          onEndEditing={addHashtag}
+        ></TextInput>
+        {post.board.pollInfo !== null && (
+          <>
+            <Spacer size={20} />
+            <Container style={{ flexDirection: "row" }}>
+              <Input
+                style={{ width: "80%", marginRight: 10, height: 35 }}
+                placeholder="투표 제목"
+                onChangeText={text => setVoteTitle(text)}
+                value={voteTitle}
+              />
+              <TextButton onPress={addVoteOptions}>추가</TextButton>
+            </Container>
+            <Spacer size={10} />
+            <Container>
+              {voteOptions.map((option, index) => (
+                <>
+                  <Container style={{ flexDirection: "row", alignItems: "center" }} key={index}>
+                    <Input
+                      style={{ flex: 1 }}
+                      key={index}
+                      placeholder={`투표 선택지 ${index + 1}`}
+                      onChangeText={text => handleVoteOptionChange(index, text)}
+                      value={option.optionName}
+                    />
+                    <TouchableOpacity onPress={() => handleRemoveVoteOption(index)}>
+                      <Text style={{ color: "black" }}>X</Text>
+                    </TouchableOpacity>
+                  </Container>
+                  <Spacer size={10} />
+                </>
+              ))}
+            </Container>
+          </>
+        )}
+        <Spacer size={10} />
+        <View style={{ flexDirection: "row" }}>
+          {hashtags.map((tag, index) => (
+            <Text key={index}>
+              <Text style={{ backgroundColor: "#C9E4F9" }}>{tag}</Text>
+              {"   "}
+            </Text>
+          ))}
+        </View>
+        <Spacer size={20} />
         <Input
           style={{ height: 500 }}
           placeholder="본문"
@@ -98,6 +265,17 @@ const Edit: React.FC = () => {
           value={body}
           multiline={true}
         ></Input>
+        <ScrollView horizontal={true} style={{ flexDirection: "row" }}>
+          <Spacer size={20} />
+          {Array.isArray(imageUrl) &&
+            imageUrl.map((uri, index) => (
+              <Image
+                key={index}
+                source={{ uri: uri }}
+                style={{ width: 200, height: 200, marginRight: 5 }} // 이미지 간 간격을 조절해줍니다.
+              />
+            ))}
+        </ScrollView>
       </Container>
     </Container>
   );
