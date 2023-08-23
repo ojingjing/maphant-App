9fd38d61366cf32b504549e39fa6ae13de167abe
ur <dorosi150@gmail.com>
2023-08-24 00:35:01 +0900

    Fix: 질문 게시판 답변 작성

--- src/App/Board/QA_answer.tsx
+++ src/App/Board/QA_answer.tsx
@@ -2,7 +2,7 @@
 import { useNavigation, useRoute } from "@react-navigation/native";
 import CheckBox from "expo-checkbox";
 import React, { useState } from "react";
-import { Alert, Text } from "react-native";
+import { Alert, KeyboardAvoidingView, Platform, Text } from "react-native";
 
 import { boardPost } from "../../Api/board";
 import { Container, Input, Spacer, TextButton } from "../../components/common";
@@ -11,8 +11,6 @@ import { NavigationProps } from "../../Navigator/Routes";
 const QA_answer: React.FC = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
-  const [hashtagInput, setHashtagInput] = useState("");
-  const [hashtags, setHashtags] = useState<string[]>([]);
   const [checkList, setCheckList] = useState<string[]>([]);
   const [isanonymous, setIsanonymous] = useState(0);
   const [isHide, setIsHide] = useState(0);
@@ -33,7 +31,7 @@ const QA_answer: React.FC = () => {
     try {
       const response = await boardPost(
         id,
-        2,
+        7,
         title,
         body,
         isHide,
@@ -48,81 +46,65 @@ const QA_answer: React.FC = () => {
     }
   };
 
-  const updateHashtags = () => {
-    const words = hashtagInput.split("");
-    const newHashtags = words.filter(word => word.startsWith("#"));
-    setHashtags(newHashtags);
-  };
-
-  const addHashtag = () => {
-    if (hashtagInput.trim() !== "") {
-      updateHashtags();
-      setHashtagInput("");
-    }
-  };
-
   return (
-    <Container isFullScreen={true}>
-      <Container style={{ flexDirection: "row", justifyContent: "space-between" }}>
-        <Container
-          style={{ flexDirection: "row", justifyContent: "space-between", paddingHorizontal: 10 }}
-        >
-          <Container style={{ flexDirection: "row", marginRight: 10 }}>
-            <CheckBox
-              style={{ marginRight: 5 }}
-              value={checkList.includes("private")}
-              onValueChange={isChecked => {
-                check("private", isChecked);
-                setIsHide(isChecked ? 1 : 0);
-              }}
-            ></CheckBox>
-            <Text>비공개</Text>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 100 : 0}
+      style={{ padding: 5, borderRadius: 5 }}
+    >
+      <Container
+        style={{ marginHorizontal: 15, marginVertical: 10 }}
+        // isForceKeyboardAvoiding={true}
+      >
+        <Container style={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <Container
+            style={{ flexDirection: "row", justifyContent: "space-between", paddingHorizontal: 10 }}
+          >
+            <Container style={{ flexDirection: "row", marginRight: 10, alignItems: "center" }}>
+              <CheckBox
+                style={{ marginRight: 5 }}
+                value={checkList.includes("private")}
+                onValueChange={isChecked => {
+                  check("private", isChecked);
+                  setIsHide(isChecked ? 1 : 0);
+                }}
+              ></CheckBox>
+              <Text>비공개</Text>
+            </Container>
+            <Container style={{ flexDirection: "row", marginRight: 10, alignItems: "center" }}>
+              <CheckBox
+                style={{ marginRight: 5 }}
+                value={checkList.includes("anonymous")}
+                onValueChange={isChecked => {
+                  check("anonymous", isChecked);
+                  setIsanonymous(isChecked ? 1 : 0);
+                }}
+              ></CheckBox>
+              <Text>익명</Text>
+            </Container>
           </Container>
           <Container style={{ flexDirection: "row" }}>
-            <CheckBox
-              style={{ marginRight: 5 }}
-              value={checkList.includes("anonymous")}
-              onValueChange={isChecked => {
-                check("anonymous", isChecked);
-                setIsanonymous(isChecked ? 1 : 0);
-              }}
-            ></CheckBox>
-            <Text>익명</Text>
+            <TextButton onPress={() => complete()}>완료</TextButton>
           </Container>
         </Container>
-        <Container style={{ flexDirection: "row" }}>
-          <TextButton onPress={() => complete()}>완료</TextButton>
+        <Container>
+          <Input
+            placeholder="제목"
+            onChangeText={text => setTitle(text)}
+            value={title}
+            // multiline={true}
+          ></Input>
+          <Spacer size={20} />
+          <Input
+            style={{ minHeight: "75%" }}
+            placeholder="본문"
+            onChangeText={text => setBody(text)}
+            value={body}
+            multiline={true}
+          ></Input>
         </Container>
       </Container>
-      <Container>
-        <Input
-          placeholder="제목"
-          onChangeText={text => setTitle(text)}
-          value={title}
-          multiline={true}
-        ></Input>
-        <Spacer size={20} />
-        <Input
-          placeholder="해시태그"
-          onChangeText={text => setHashtagInput(text)}
-          value={hashtagInput}
-          multiline={true}
-          onSubmitEditing={addHashtag}
-        ></Input>
-        <Spacer size={10} />
-        {hashtags.map((tag, index) => (
-          <Text key={index}>{tag}</Text>
-        ))}
-        <Spacer size={20} />
-        <Input
-          style={{ height: 500 }}
-          placeholder="본문"
-          onChangeText={text => setBody(text)}
-          value={body}
-          multiline={true}
-        ></Input>
-      </Container>
-    </Container>
+    </KeyboardAvoidingView>
   );
 };
 
